fbaf453e8b95de1de40609ae7dac8023cc48755b
ElmerAvila <elmer_av09@hotmail.com>
2021-12-05 19:02:06 -0600

    Documentación/Comentarios de código en C#

--- EscenarioDelReto/Assets/Scripts/Handler.cs
+++ EscenarioDelReto/Assets/Scripts/Handler.cs
@@ -9,10 +9,9 @@ using Random=UnityEngine.Random;
 
 public class Handler : MonoBehaviour
 {
-    // Use this for initialization
+    // Inicialización de variables.
     public GameObject[] carPrefab;
     public GameObject parkingSlot;
-    //private GameObject[] parkings;
     System.Threading.Thread SocketThread;
     volatile bool keepReading = true;
     string data = null;
@@ -20,18 +19,23 @@ public class Handler : MonoBehaviour
     bool simulationFinished = false;
     float time;
 
+    // Funcion Start, llamada al comienza de la simulación.
     void Start()
     {
         Application.runInBackground = true;
         startServer();
     }
 
+    // Funcion Update, llamada en cada frame de la simulación.
     void Update()
     {
+        // Control de creación de agentes, mientras que aun se reciban datos desde python llamar a la funcion que controla agentes.
         if (data != null && callAgentController){
             callAgentController = false;
             agentController();
         }
+
+        // Una vez que ya no se reciban datos y la simulación llegue a su step final se destruyen los agentes de estacionamiento.
         if (simulationFinished){
             GameObject[] parkings = GameObject.FindGameObjectsWithTag("ParkingLot");
             foreach (GameObject park in parkings){
@@ -41,6 +45,7 @@ public class Handler : MonoBehaviour
         }
     }
 
+    // Funcion que inicializa el socket que funge como servidor. 
     void startServer()
     {
         SocketThread = new System.Threading.Thread(networkCode);
@@ -48,6 +53,7 @@ public class Handler : MonoBehaviour
         SocketThread.Start();
     }
 
+    // Funcion que obtiene la direccion IP.
     private string getIPAddress()
     {
         IPHostEntry host;
@@ -68,40 +74,42 @@ public class Handler : MonoBehaviour
     Socket listener;
     Socket handler;
 
+    // Funcion que recibe la información la información a través del socket.
     void networkCode()
     {
         try {
-            // Data buffer for incoming data.
+            // Array de bytes para los datos entrantes.
             byte[] bytes = new Byte[1024];
 
-            // host running the application.
-            //Create EndPoint
-            IPAddress IPAdr = IPAddress.Parse("127.0.0.1"); // DirecciÃ³n IP
+            //Creación del EndPoint.
+            IPAddress IPAdr = IPAddress.Parse("127.0.0.1"); // Dirección IP
             IPEndPoint localEndPoint = new IPEndPoint(IPAdr, 1101);
 
-            // Create a TCP/IP socket.
+            // Creacion de TCP/IP socket.
             listener = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
 
             listener.Bind(localEndPoint);
             listener.Listen(10);
-       
+
+            // Ciclo para la lectura de infomación.
 			while (keepReading) {	
-                // Program is suspended while waiting for an incoming connection.
-                Debug.Log("Waiting for Connection");     //It works
+                
+                Debug.Log("Waiting for Connection");
 
                 handler = listener.Accept();
-                Debug.Log("Client Connected");     //It doesn't work
+                Debug.Log("Client Connected");
 
+                // Respuesta al cliente de conexion exitosa.
                 byte[] SendBytes = System.Text.Encoding.Default.GetBytes("Successful Connection");
-                handler.Send(SendBytes); // dar al cliente
+                handler.Send(SendBytes);
 
                 int bytesRec;
 					
-                // Read incomming stream into byte arrary. 					
+                // Lectura de los datos entrantes.			
                 while ((bytesRec = handler.Receive(bytes)) != 0) {
                     var incommingData = new byte[bytesRec];
                     Array.Copy(bytes, 0, incommingData, 0, bytesRec);
-                    // Convert byte array to string message.
+                    // Conversion del arreglo de bytes a string.
                     string serverMessage = System.Text.Encoding.ASCII.GetString(incommingData);
                     data = serverMessage;
                     Debug.Log("Message received as: " + serverMessage);
@@ -117,23 +125,25 @@ public class Handler : MonoBehaviour
 		}
     }
 
-    
+    // Funcion que se encarga de instanciar y asignar informacion a los agentes.
     void agentController(){
+
+        // Manejo de los datos recibidos para la creacion de coordenadas.
         string[] totalData = data.Split(':');
 
         if (totalData[0] == "vehicles"){
             string[] paths = totalData[1].Split('!');
             
-            // Iteracion sobre paths, cantidad de instancias a crear
+            // Iteracion sobre paths, cantidad de instancias a crear.
             for(int i = 0; i < paths.Length - 1; i++)
             {
-                // Obtencion de las coordenadas enviadas desde Python (para 1 instancia)
+                // Obtencion de las coordenadas enviadas desde Python (para 1 instancia).
                 string[] cords = paths[i].Split('/');
 
                 int numSteps = cords.Length;
                 Vector3[] path = new Vector3[numSteps];
                 
-                // Creacion del path para la instancia de la iteracion actual
+                // Creacion del path para la instancia de la iteracion actual.
                 int idx = 0;
                 for(int j = 0; j < cords.Length - 1; j++){     
                     string[] cor = cords[j].Split(',');
@@ -143,15 +153,18 @@ public class Handler : MonoBehaviour
 
                 int vInt = Random.Range(0, 7);
 
+                // Instanciacion de agentes.
                 GameObject car = Instantiate(carPrefab[vInt], path[0], Quaternion.identity);
                 car.GetComponent<Movement>().path = path;
                 car.GetComponent<Movement>().timeStep = time;
             }
-        }
+        } // Manejo de infomación inicial para la generacion de estacionamientos.
         else if (totalData[0] == "parkings"){
             string[] parkingsPos = totalData[1].Split('/');
             foreach (var park in parkingsPos){
                 string[] cor = park.Split(',');
+
+                // Instanciacion de los agentes estacionamiento.
                 GameObject parking = Instantiate(parkingSlot, new Vector3(float.Parse(cor[0]) * 10 - 3f, 4.29f, float.Parse(cor[1]) * 10 -3f), Quaternion.identity);
                 parking.tag = "ParkingLot";
             }
@@ -165,10 +178,11 @@ public class Handler : MonoBehaviour
         callAgentController = true;
     }
 
+    // Funcion que aborta o desconecta el socket en dependencia a la situacion. 
     void stopServer(){
         keepReading = false;
 
-        //stop thread
+        // Detencion del socket.
         if (SocketThread != null)
         {
             SocketThread.Abort();
--- EscenarioDelReto/Assets/Scripts/Movement.cs
+++ EscenarioDelReto/Assets/Scripts/Movement.cs
@@ -9,6 +9,7 @@ using System.Threading;
 
 public class Movement : MonoBehaviour
 {
+    // Inicializacion de variables.
     public Vector3[] path;
     private int step = 1;
     private float speed;
@@ -16,21 +17,24 @@ public class Movement : MonoBehaviour
     private Vector3 current;
     private Vector3 next;
 
-    // Start is called before the first frame update
+    // Función Start, llamada al comienzo de la simulación.
     void Start()
     {
         speed = 10/timeStep;
+        // Llamada repetitica de la funcion stepController cada segundo.
         InvokeRepeating("stepController", 0.0f, timeStep);
     }
 
-    // Update is called once per frame
+    // Función Update, llamada en cada frame.
     void Update()
     {
+        // Reakizacion de la tranlación del agente al siguiente punto del patrón.
         if (next != current){
             transform.Translate(new Vector3(0,0,speed*Time.deltaTime));
         }
     }
 
+    // Funcion que controla la dirección y posicion de los agentes durante el recorrido de su patron.
     void stepController(){
         current = path[step];
         transform.position = current;
@@ -39,6 +43,8 @@ public class Movement : MonoBehaviour
             next = path[step];
             transform.LookAt(next);
         } else{
+
+            // Eliminacion del agente una vez que haya finalizado su recorrido.
             Destroy(gameObject,.0f);
             Debug.Log("Recorrido Finalizado. Destruccion de Vehículo");
         }
